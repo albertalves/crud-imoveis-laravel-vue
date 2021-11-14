ba62222e7d8ae24d274b4a4a7fe96495bb5f65c5
ALBERT-PC\Albert Alves <albert@ezoom.com.br>
2021-11-13 21:36:00 -0300

    Namespace v1 e renomeando colunas da tabela Contratos

--- database/migrations/2021_11_11_183539_create_contratos_table.php
+++ database/migrations/2021_11_11_183539_create_contratos_table.php
@@ -19,8 +19,8 @@ class CreateContratosTable extends Migration
             $table->foreignId('propriedade_id')->constrained('propriedades');
             $table->string('tipo_pessoa', 45);
             $table->string('documento', 45);
-            $table->string('email', 255);
-            $table->string('nome_completo', 150);
+            $table->string('email_contratante', 255);
+            $table->string('nome_completo_contratante', 150);
             $table->timestamps();
         });
     }
--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,18 @@
 <?php
 
-use App\Http\Controllers\Api\{
-    PropriedadeController,
+use App\Http\Controllers\Api\v1\{
+    Propriedade\PropriedadeController,
+    Contrato\ContratoController
 };
 
 use Illuminate\Support\Facades\Route;
 
-Route::put('/propriedade/{propriedade}', [PropriedadeController::class, 'update']);
-Route::delete('/propriedade/{uuid}', [PropriedadeController::class, 'destroy']);
-Route::get('/propriedade/{uuid}', [PropriedadeController::class, 'show']);
-Route::post('/propriedades', [PropriedadeController::class, 'store']);
-Route::get('/propriedades', [PropriedadeController::class, 'index']);
+Route::prefix('/v1')->group(function () {
+    Route::put('/propriedade/{uuid}', [PropriedadeController::class, 'update']);
+    Route::delete('/propriedade/{uuid}', [PropriedadeController::class, 'destroy']);
+    Route::get('/propriedade/{uuid}', [PropriedadeController::class, 'show']);
+    Route::post('/propriedades', [PropriedadeController::class, 'store']);
+    Route::get('/propriedades', [PropriedadeController::class, 'index']);
+
+    Route::post('/contratos', [ContratoController::class, 'store']);
+});
