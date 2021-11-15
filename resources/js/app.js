4baaaad0796e4bf3ad33778d454974ba1cda0dd9
ALBERT-PC\Albert Alves <albert@ezoom.com.br>
2021-11-15 20:23:47 -0300

    Instalação e configuração das dependencias

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,30 @@
 require('./bootstrap');
+
+window.Vue = require('vue').default;
+
+import router from './routes';
+import store from './vuex';
+import vuetify from './vuetify';
+import snotify from 'vue-snotify';
+import VueTheMask from 'vue-the-mask';
+
+const options = {
+    toast: {
+        showProgressBar: false,
+        timeout: 5000
+    }
+}
+
+Vue.use(VueTheMask);
+Vue.use(snotify, options);
+
+Vue.component('preloader-component', require('./components/comum/PreloaderComponent.vue').default);
+
+const app = new Vue({
+    el: '#app',
+    router,
+    store,
+    vuetify
+});
+
+store.dispatch('checkLogin').then(() => router.push(store.state.auth.urlBack));
